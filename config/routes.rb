ce0b02e49778eb0d8fe30550bc073c7c4e203b81
danielcorrea <danielcorrea@claretiano.edu.br>
2021-08-11 22:55:35 -0300

    Configurando admin#edit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' # Dashboard
+    resources :admins , only:[:index, :edit] # Administradores
   end
   devise_for :users
   devise_for :admins
